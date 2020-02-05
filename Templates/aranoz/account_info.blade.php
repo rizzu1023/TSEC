595cddbbe93d64dd01cd1456a24eeaff75f3972a
cshoaib05 <choudharyshoaib05@gmail.com>
2020-02-05 22:18:28 +0530

    changes

--- Templates/aranoz/account_info.blade.php
+++ Templates/aranoz/account_info.blade.php
@@ -6,27 +6,27 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <title>aranaz</title>
-  <link rel="icon" href="img/favicon.png">
+  <link rel="icon" href="{{asset('Main/img/favicon.png')}}">
   <!-- Bootstrap CSS -->
-  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="{{asset('Main/css/bootstrap.min.css')}}">
   <!-- animate CSS -->
-  <link rel="stylesheet" href="css/animate.css">
+  <link rel="stylesheet" href="{{asset('Main/css/animate.css')}}">
   <!-- owl carousel CSS -->
-  <link rel="stylesheet" href="css/owl.carousel.min.css">
+  <link rel="stylesheet" href="{{asset('Main/css/owl.carousel.min.css')}}">
   <!-- nice select CSS -->
-  <link rel="stylesheet" href="css/nice-select.css">
+  <link rel="stylesheet" href="{{asset('Main/css/nice-select.css')}}">
   <!-- font awesome CSS -->
-  <link rel="stylesheet" href="css/all.css">
+  <link rel="stylesheet" href="{{asset('Main/css/all.css')}}">
   <!-- flaticon CSS -->
-  <link rel="stylesheet" href="css/flaticon.css">
-  <link rel="stylesheet" href="css/themify-icons.css">
+  <link rel="stylesheet" href="{{asset('Main/css/flaticon.css')}}">
+  <link rel="stylesheet" href="{{asset('Main/css/themify-icons.css')}}">
   <!-- font awesome CSS -->
-  <link rel="stylesheet" href="css/magnific-popup.css">
+  <link rel="stylesheet" href="{{asset('Main/css/magnific-popup.css')}}">
   <!-- swiper CSS -->
-  <link rel="stylesheet" href="css/slick.css">
-  <link rel="stylesheet" href="css/price_rangs.css">
+  <link rel="stylesheet" href="{{asset('Main/css/slick.css')}}">
+  <link rel="stylesheet" href="{{asset('Main/css/price_rangs.css')}}">
   <!-- style CSS -->
-  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="{{asset('Main/css/style.css')}}">
 </head>
 
 <body>
@@ -36,7 +36,7 @@
         <div class="row align-items-center">
             <div class="col-lg-12">
                 <nav class="navbar navbar-expand-lg navbar-light">
-                    <a class="navbar-brand" href="index.html"> <img src="img/logo.png" alt="logo"> </a>
+                    <a class="navbar-brand" href="index.html"> <img src="{{asset('Main/img/logo.png')}}" alt="logo"> </a>
                     <button class="navbar-toggler" type="button" data-toggle="collapse"
                         data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                         aria-expanded="false" aria-label="Toggle navigation">
@@ -310,33 +310,33 @@ Copyright &copy;<script>document.write(new Date().getFullYear());</script> All r
 
   <!-- jquery plugins here-->
   <!-- jquery -->
-  <script src="js/jquery-1.12.1.min.js"></script>
+  <script src="{{asset('Main/js/jquery-1.12.1.min.js')}}"></script>
   <!-- popper js -->
-  <script src="js/popper.min.js"></script>
+  <script src="{{asset('Main/js/popper.min.js')}}"></script>
   <!-- bootstrap js -->
-  <script src="js/bootstrap.min.js"></script>
+  <script src="{{asset('Main/js/bootstrap.min.js')}}"></script>
   <!-- easing js -->
-  <script src="js/jquery.magnific-popup.js"></script>
+  <script src="{{asset('Main/js/jquery.magnific-popup.js')}}"></script>
   <!-- swiper js -->
-  <script src="js/swiper.min.js"></script>
+  <script src="{{asset('Main/js/swiper.min.js')}}"></script>
   <!-- swiper js -->
-  <script src="js/masonry.pkgd.js"></script>
+  <script src="{{asset('Main/js/masonry.pkgd.js')}}"></script>
   <!-- particles js -->
-  <script src="js/owl.carousel.min.js"></script>
-  <script src="js/jquery.nice-select.min.js"></script>
+  <script src="{{asset('Main/js/owl.carousel.min.js')}}"></script>
+  <script src="{{asset('Main/js/jquery.nice-select.min.js')}}"></script>
   <!-- slick js -->
-  <script src="js/slick.min.js"></script>
-  <script src="js/jquery.counterup.min.js"></script>
-  <script src="js/waypoints.min.js"></script>
-  <script src="js/contact.js"></script>
-  <script src="js/jquery.ajaxchimp.min.js"></script>
-  <script src="js/jquery.form.js"></script>
-  <script src="js/jquery.validate.min.js"></script>
-  <script src="js/mail-script.js"></script>
-  <script src="js/stellar.js"></script>
-  <script src="js/price_rangs.js"></script>
+  <script src="{{asset('Main/js/slick.min.js')}}"></script>
+  <script src="{{asset('Main/js/jquery.counterup.min.js')}}"></script>
+  <script src="{{asset('Main/js/waypoints.min.js')}}"></script>
+  <script src="{{asset('Main/js/contact.js')}}"></script>
+  <script src="{{asset('Main/js/jquery.ajaxchimp.min.js')}}"></script>
+  <script src="{{asset('Main/js/jquery.form.js')}}"></script>
+  <script src="{{asset('Main/js/jquery.validate.min.js')}}"></script>
+  <script src="{{asset('Main/js/mail-script.js')}}"></script>
+  <script src="{{asset('Main/js/stellar.js')}}"></script>
+  <script src="{{asset('Main/js/price_rangs.js')}}"></script>
   <!-- custom js -->
-  <script src="js/custom.js"></script>
+  <script src="{{asset('Main/js/custom.js')}}"></script>
 </body>
 
 </html>
